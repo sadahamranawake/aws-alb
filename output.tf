214162500a81a7049fa553a964fd2c420de1eb38
Sadaham Ranawake <sadahamrc@gmail.com>
2023-04-20 16:59:35 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "AlbArn" {
   value = aws_lb.AlbInternetFacing.arn
 }
+
+output "AlbDnsName" {
+  value = aws_lb.AlbInternetFacing.dns_name
+}
+
+output "AlbZoneId" {
+  value = aws_lb.AlbInternetFacing.zone_id
+}
